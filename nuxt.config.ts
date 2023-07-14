f9f9fc773e412a3baa961ff277ae47b45e3e4c79
Tomasz Sroka <tsroka41@gmail.com>
2023-07-14 15:19:51 +0200

    added bare eslint and pinia modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
   vite: {
     server: {
       fs: {
@@ -14,4 +14,5 @@ export default defineNuxtConfig({
   css: [
     '@/assets/css/main.css',
   ],
+
 })
