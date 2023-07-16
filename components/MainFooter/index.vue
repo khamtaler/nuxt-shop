6e28c47225716eea3898ac0986d6ce7c1abc6bd0
Tomasz Sroka <tsroka41@gmail.com>
2023-07-16 19:20:33 +0200

    added front page banner with gradient overlay

--- components/MainFooter/index.vue
+++ components/MainFooter/index.vue
@@ -1,47 +1,53 @@
 <template>
-  <footer class="mx-auto my-10 flex max-w-6xl justify-around">
-    <div class="flex w-[22%] flex-col">
-      <!-- logo component-->
-      <nuxt-link to="/">LOGO</nuxt-link>
-    </div>
-    <ul class="flex w-[22%] flex-col">
-      <MainFooterBaseLink
-        v-for="item in itemsList"
-        :link="item.link"
-        :name="item.text"
-        class="capital block"
-      >
-        {{ item.text }}
-      </MainFooterBaseLink>
-    </ul>
-    <ul class="flex w-[22%] flex-col">
-      <MainFooterBaseLink
-        v-for="item in itemsList"
-        :link="item.link"
-        :name="item.text"
-        class="capital block"
-      >
-        {{ item.text }}
-      </MainFooterBaseLink>
-    </ul>
-    <div class="block w-[22%]">
-      <client-only>
-        <nuxt-link to="/" target="_blank">
-          <font-awesome-icon
-            :icon="['fab', 'facebook']"
-            class="mx-2 text-4xl"
-          />
-        </nuxt-link>
-        <nuxt-link to="/" target="_blank">
-          <font-awesome-icon
-            :icon="['fab', 'instagram']"
-            class="mx-2 text-4xl"
-          />
-        </nuxt-link>
-        <nuxt-link to="/" target="_blank">
-          <font-awesome-icon :icon="['fab', 'twitter']" class="mx-2 text-4xl" />
-        </nuxt-link>
-      </client-only>
+  <footer class="border-t-2">
+    <div class="mx-auto my-10 flex max-w-6xl justify-around">
+      <div class="flex w-[22%] flex-col">
+        <!-- logo component-->
+        <nuxt-link to="/">LOGO</nuxt-link>
+      </div>
+      <ul class="flex w-[22%] flex-col">
+        <MainFooterBaseLink
+          v-for="item in itemsList"
+          :link="item.link"
+          :name="item.text"
+          class="capital block"
+        >
+          {{ item.text }}
+        </MainFooterBaseLink>
+      </ul>
+      <ul class="flex w-[22%] flex-col">
+        <MainFooterBaseLink
+          v-for="item in itemsList"
+          :link="item.link"
+          :name="item.text"
+          class="capital block"
+        >
+          {{ item.text }}
+        </MainFooterBaseLink>
+      </ul>
+      <div class="block w-[22%]">
+        <h5 class="mb-3">Socials</h5>
+        <client-only>
+          <nuxt-link to="/" target="_blank">
+            <font-awesome-icon
+              :icon="['fab', 'facebook-f']"
+              class="mr-2 text-4xl"
+            />
+          </nuxt-link>
+          <nuxt-link to="/" target="_blank">
+            <font-awesome-icon
+              :icon="['fab', 'instagram']"
+              class="mx-2 text-4xl"
+            />
+          </nuxt-link>
+          <nuxt-link to="/" target="_blank">
+            <font-awesome-icon
+              :icon="['fab', 'twitter']"
+              class="ml-2 text-4xl"
+            />
+          </nuxt-link>
+        </client-only>
+      </div>
     </div>
   </footer>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Main Test</div>
+  <div class="min-h-[100vh]">
+    <div
+      class="grid min-h-[400px] items-stretch justify-stretch bg-[url('@/public/images/wallpaper.png')]"
+    >
+      <div class="grid h-full w-full grid-cols-2 bg-gradient-to-r from-black">
+        <div class="p-6 text-white">
+          <h2 class="text-5xl">test</h2>
+          <p>U nas se kupisz co tam se chcesz</p>
+        </div>
+      </div>
+    </div>
+    <!-- <figure>
+      <img
+        src="@/public/images/wallpaper.png"
+        class="max-h-[500px] w-full object-cover"
+      />
+    </figure> -->
+  </div>
 </template>
 
 <script setup></script>
