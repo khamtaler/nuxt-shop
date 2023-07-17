1cbc861d6992053d20b407e00eaada708b1c559d
Tomasz Sroka <tsroka41@gmail.com>
2023-07-17 12:57:32 +0200

    adding few products mock and base product tile component

---
+++ components/ShopItemTile.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <figure><img :src="props.imageUrl" :alt="props.text" /></figure>
+    <h6>{{ props.text }}</h6>
+    <p>
+      <span :class="isDiscounted ? 'line-through' : ''">{{ props.price }}</span>
+      <span v-if="props.price > props.promotionPrice">
+        {{ props.promotionPrice }}
+      </span>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  sku: string
+  imageUrl: string
+  text: string
+  price: number
+  promotionPrice: number
+}
+const props = defineProps<Props>()
+
+const isDiscounted = computed(() => {
+  return props.price > props.promotionPrice ? true : false
+})
+</script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-[100vh]">
     <div
-      class="grid min-h-[400px] items-stretch justify-stretch bg-[url('@/public/images/wallpaper.png')]"
+      class="grid min-h-[400px] items-stretch justify-stretch bg-[url('@/images/wallpaper.png')]"
     >
       <div
         class="flex h-full w-full items-center justify-center bg-gradient-to-r from-transparent via-black to-transparent"
@@ -16,10 +16,21 @@
         </div>
       </div>
     </div>
+    <div>
+      <ShopItemTile
+        v-for="item in productsList"
+        :key="item.sku"
+        :sku="item.sku"
+        :imageUrl="item.imageUrl"
+        :text="item.text"
+        :price="item.price"
+        :promotionPrice="item.promotionPrice"
+      />
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const welcomeText = 'Welcome'
 const greetingsText = 'Scroll down the page to find our products :)'
 let i = 0
@@ -27,37 +38,41 @@ let j = 0
 const speed = 100
 
 function typeWriterWelcome() {
-  if (i < welcomeText.length) {
+  if (i < welcomeText.length && welcomeText) {
+    let welcome = document.querySelector('#Welcome') as HTMLHeadingElement
     if (greetingsText.charAt(i) === ' ') {
-      document.querySelector('#Welcome').innerHTML += welcomeText.charAt(i)
+      welcome.innerHTML += welcomeText.charAt(i)
       i++
       setTimeout(typeWriterWelcome, 0)
       return
     }
-    document.querySelector('#Welcome').innerHTML += welcomeText.charAt(i)
+
+    welcome.innerHTML += welcomeText.charAt(i)
     i++
     setTimeout(typeWriterWelcome, speed)
   }
 }
 
 function typeWriterGreetings() {
-  if (j < greetingsText.length) {
+  if (j < greetingsText.length && greetingsText) {
+    let greetings = document.querySelector('#Greetings') as HTMLHeadingElement
     if (greetingsText.charAt(j) === ' ') {
-      document.querySelector('#Greetings').innerHTML += greetingsText.charAt(j)
+      greetings.innerHTML += greetingsText.charAt(j)
       j++
       setTimeout(typeWriterGreetings, 100)
       return
     }
-    document.querySelector('#Greetings').innerHTML += greetingsText.charAt(j)
+
+    greetings.innerHTML += greetingsText.charAt(j)
     j++
     setTimeout(typeWriterGreetings, getRandomIntInclusive(50, 200))
   }
 }
 
-function getRandomIntInclusive(min, max) {
+function getRandomIntInclusive(min: number, max: number) {
   min = Math.ceil(min)
   max = Math.floor(max)
-  return Math.floor(Math.random() * (max - min + 1) + min) // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 onMounted(() => {
@@ -66,6 +81,52 @@ onMounted(() => {
     typeWriterGreetings()
   }, speed * welcomeText.length)
 })
+
+interface ProductItem {
+  sku: String
+  imageUrl: String
+  text: String
+  price: Number
+  promotionPrice: Number
+}
+
+const productsList: ProductItem[] = [
+  {
+    sku: '1',
+    imageUrl: '/',
+    text: 'Product 1',
+    price: 100,
+    promotionPrice: 90,
+  },
+  {
+    sku: '2',
+    imageUrl: '/',
+    text: 'Product 2',
+    price: 900,
+    promotionPrice: 900,
+  },
+  {
+    sku: '3',
+    imageUrl: '/',
+    text: 'Product 3',
+    price: 800,
+    promotionPrice: 400,
+  },
+  {
+    sku: '4',
+    imageUrl: '/',
+    text: 'Product 4',
+    price: 110,
+    promotionPrice: 90,
+  },
+  {
+    sku: '5',
+    imageUrl: '/',
+    text: 'Product 5',
+    price: 110,
+    promotionPrice: 90,
+  },
+]
 </script>
 
 <style lang="scss" scoped></style>
