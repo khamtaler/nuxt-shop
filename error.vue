0822425eba08c52c988bf57fe655be6a7cc5898d
Tomasz Sroka <tsroka41@gmail.com>
2023-07-21 12:55:07 +0200

    adding base error page

---
+++ error.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex min-h-[500px] items-center justify-center">
+    <div class="text-center">
+      <h1 class="mb-5 text-5xl">{{ error.statusCode }}</h1>
+      <h2 class="mb-5 text-4xl">Opsss</h2>
+      <p class="mb-5 text-xl">{{ error.message }}</p>
+      <button class="bg-black px-5 py-3 text-white">Return Home...</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps(['error'])
+</script>
+
+<style scoped></style>
