ccd7875cfbc5abcf4f35b7aa732974ea8c9aa964
Tomasz Sroka <tsroka41@gmail.com>
2023-07-30 15:46:35 +0200

    login modal styles

--- components/Account/Login.vue
+++ components/Account/Login.vue
@@ -1,12 +1,12 @@
 <template>
   <form
     ref="login"
-    class="preserve3d mx-auto mt-10 flex flex-col rounded-lg bg-[#000000] p-28 shadow-lg transition-transform duration-500 md:mt-0 md:w-1/2 lg:w-3/6"
+    class="preserve3d mx-auto mt-10 flex flex-col rounded-lg border-2 border-lightblue bg-darkblue p-28 shadow-lg transition-transform duration-500 md:mt-0 md:w-1/2 lg:w-3/6"
   >
-    <h2 class="mb-5 text-lg font-medium text-[#aac8e4]">Login</h2>
+    <h2 class="mb-5 text-2xl font-medium text-white">Login</h2>
     <!-- Email input -->
     <div class="relative mb-4">
-      <label for="email" class="text-sm leading-7 text-gray-400">Email</label>
+      <label for="email" class="text-sm leading-7 text-gray-200">Email</label>
       <div class="relative">
         <input
           :disabled="props.open ? false : true"
@@ -14,13 +14,13 @@
           id="email"
           name="email"
           placeholder="e.g. example@email.com"
-          class="w-full rounded border border-gray-600 bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out placeholder:text-gray-500 focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+          class="w-full rounded border border-white bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out placeholder:text-gray-300 focus:border-orange-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
         />
       </div>
     </div>
     <!-- Password input -->
     <div class="relative mb-4">
-      <label for="password" class="text-sm leading-7 text-gray-400">
+      <label for="password" class="text-sm leading-7 text-gray-200">
         Password
       </label>
       <div class="relative">
@@ -29,7 +29,7 @@
           type="password"
           id="password"
           name="password"
-          class="w-full rounded border border-gray-600 bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+          class="w-full rounded border border-white bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-orange-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
         />
       </div>
     </div>
@@ -37,13 +37,15 @@
     <button
       :disabled="props.open ? false : true"
       type="submit"
-      class="mt-5 rounded border-0 bg-blue-500 px-8 py-2 font-bold text-[#213547] transition-colors duration-500 hover:bg-blue-600 focus:outline-none"
+      class="mt-10 rounded border-0 bg-orange-500 px-8 py-2 font-bold text-white transition-colors duration-500 hover:bg-lightblue focus:outline-none"
     >
       Login
     </button>
-    <p class="mt-5">
+    <p class="mt-2 text-gray-200">
       Not having an account?
-      <span @click="loginStore.toggleLoginRegister">Register!</span>
+      <span @click="loginStore.toggleLoginRegister" class="font-bold">
+        Register!
+      </span>
     </p>
   </form>
 </template>
--- components/Account/Register.vue
+++ components/Account/Register.vue
@@ -1,12 +1,12 @@
 <template>
   <form
     ref="register"
-    class="preserve3d rotateX-180 mx-auto mt-10 flex flex-col rounded-lg bg-[#000000] p-28 shadow-lg transition-transform duration-500 md:mt-0 md:w-1/2 lg:w-3/6"
+    class="preserve3d rotateX-180 mx-auto mt-10 flex flex-col rounded-lg border-2 border-orange-500 bg-darkblue p-28 shadow-lg transition-transform duration-500 md:mt-0 md:w-1/2 lg:w-3/6"
   >
-    <h2 class="mb-5 text-lg font-medium text-[#aac8e4]">Register</h2>
+    <h2 class="mb-5 text-2xl font-medium text-white">Register</h2>
     <!-- Email input -->
     <div class="relative mb-4">
-      <label for="email" class="text-sm leading-7 text-gray-400">Email</label>
+      <label for="email" class="text-sm leading-7 text-gray-200">Email</label>
       <div class="relative">
         <input
           :disabled="props.open ? false : true"
@@ -14,13 +14,13 @@
           id="email"
           name="email"
           placeholder="e.g. example@email.com"
-          class="w-full rounded border border-gray-600 bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out placeholder:text-gray-500 focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+          class="w-full rounded border border-white bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out placeholder:text-gray-300 focus:border-orange-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
         />
       </div>
     </div>
     <!-- Password input -->
     <div class="relative mb-4">
-      <label for="password" class="text-sm leading-7 text-gray-400">
+      <label for="password" class="text-sm leading-7 text-gray-200">
         Password
       </label>
       <div class="relative">
@@ -29,13 +29,13 @@
           type="password"
           id="password"
           name="password"
-          class="w-full rounded border border-gray-600 bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+          class="w-full rounded border border-white bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-orange-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
         />
       </div>
     </div>
     <!-- Confirm Password input -->
     <div class="relative mb-4">
-      <label for="confirmPassword" class="text-sm leading-7 text-gray-400">
+      <label for="confirmPassword" class="text-sm leading-7 text-gray-200">
         Confirm password
       </label>
       <div class="relative">
@@ -44,20 +44,22 @@
           type="password"
           id="confirmPassword"
           name="confirmPassword"
-          class="w-full rounded border border-gray-600 bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
+          class="w-full rounded border border-white bg-transparent bg-opacity-20 px-3 py-1 text-base leading-8 text-gray-100 outline-none transition-colors duration-200 ease-in-out focus:border-blue-500 focus:bg-transparent focus:ring-2 focus:ring-transparent"
         />
       </div>
     </div>
     <button
       :disabled="props.open ? false : true"
       type="submit"
-      class="mt-10 rounded border-0 bg-blue-500 px-8 py-2 font-bold text-[#213547] transition-colors duration-500 hover:bg-blue-600 focus:outline-none"
+      class="mt-10 rounded border-0 bg-orange-500 px-8 py-2 font-bold text-white transition-colors duration-500 hover:bg-lightblue focus:outline-none"
     >
       Submit
     </button>
-    <p>
+    <p class="mt-2 text-gray-200">
       Already got account?
-      <span @click="loginStore.toggleLoginRegister">Sign in!</span>
+      <span @click="loginStore.toggleLoginRegister" class="font-bold">
+        Sign in!
+      </span>
     </p>
   </form>
 </template>
