30def282b36b7db92eeecd57eb40ea7a912649ee
Tomasz Sroka <tsroka41@gmail.com>
2023-07-31 12:50:46 +0200

    final touch on checkout form

--- components/Checkout/Form.vue
+++ components/Checkout/Form.vue
@@ -1,84 +1,165 @@
 <template>
-  <div class="grid grid-cols-12 rounded-md border-2 border-lightpurple p-5">
-    <div class="col-start-1 col-end-7 my-2 flex flex-col">
-      <label class="mb-2" for="firstName">First Name</label>
+  <div
+    class="checkout grid grid-cols-12 rounded-md border-2 border-lightpurple p-5"
+  >
+    <div class="relative col-start-1 col-end-7 my-2 flex flex-col">
+      <label
+        class="absolute mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.firstName.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="firstName"
+      >
+        First Name
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="First Name"
         name="firstName"
         v-model="formData.firstName"
       />
     </div>
-    <div class="col-start-8 col-end-13 my-2 flex flex-col">
-      <label class="mb-2" for="lastName">Last Name</label>
+    <div class="relative col-start-8 col-end-13 my-2 flex flex-col">
+      <label
+        class="dura absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.lastName.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="lastName"
+      >
+        Last Name
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="Last Name"
         name="lastName"
         v-model="formData.lastName"
+        :class="formData.lastName ? 'top-[-5px]' : 'top-3'"
       />
     </div>
-    <div class="col-start-1 col-end-13 my-2 flex flex-col">
-      <label class="mb-2" for="email">Email</label>
+    <div class="relative col-start-1 col-end-13 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.email.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="email"
+      >
+        Email
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="email"
-        placeholder="Email"
         name="email"
         v-model="formData.email"
+        :class="formData.email ? 'top-[-5px]' : 'top-3'"
       />
     </div>
-    <div class="col-start-1 col-end-7 my-2 flex flex-col">
-      <label class="mb-2" for="street">street</label>
+    <div class="relative col-start-1 col-end-7 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.street.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="street"
+      >
+        street
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="Street"
         name="street"
         v-model="formData.street"
+        :class="formData.street ? 'top-[-5px]' : 'top-3'"
       />
     </div>
-    <div class="col-start-8 col-end-13 my-2 flex flex-col">
-      <label class="mb-2" for="houseNumber">House number</label>
+    <div class="relative col-start-8 col-end-13 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.houseNumber.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="houseNumber"
+      >
+        House number
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="house number"
         name="houseNumber"
         v-model="formData.houseNumber"
+        :class="formData.houseNumber ? 'top-[-5px]' : 'top-3'"
       />
     </div>
 
-    <div class="col-start-1 col-end-7 my-2 flex flex-col">
-      <label class="mb-2" for="city">City</label>
+    <div class="relative col-start-1 col-end-7 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.city.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="city"
+      >
+        City
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="City"
         name="city"
         v-model="formData.city"
+        :class="formData.city ? 'top-[-5px]' : 'top-3'"
       />
     </div>
-    <div class="col-start-8 col-end-13 my-2 flex flex-col">
-      <label class="mb-2" for="postalCode">Postal code</label>
+    <div class="relative col-start-8 col-end-13 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.postalCode.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="postalCode"
+      >
+        Postal code
+      </label>
       <input
-        class="rounded-md border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="rounded-md border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="Postal code"
         name="postalCode"
         v-model="formData.postalCode"
+        :class="formData.postalCode ? 'top-[-5px]' : 'top-3'"
       />
     </div>
-    <div class="col-start-1 col-end-13 my-2 flex flex-col">
-      <label class="mb-2" for="phoneNumber">Phone number</label>
+    <div class="relative col-start-1 col-end-13 my-2 flex flex-col">
+      <label
+        class="absolute left-1 mb-2 px-2 transition-all duration-75"
+        :class="
+          formData.phoneNumber.length > 0
+            ? 'left-3  top-[-12px] z-10 rounded-md bg-white text-sm'
+            : 'text-md left-1 top-2 z-[-1]'
+        "
+        for="phoneNumber"
+      >
+        Phone number
+      </label>
       <input
-        class="border-2 border-gray-200 p-2 focus:border-lightpurple"
+        class="border-2 border-gray-200 bg-transparent p-2 focus:border-lightpurple"
         type="text"
-        placeholder="Phone number"
         name="phoneNumber"
         v-model="formData.phoneNumber"
+        :class="formData.phoneNumber ? 'top-[-5px]' : 'top-3'"
       />
     </div>
     <div class="col-start-1 col-end-13 mt-5 flex items-center justify-center">
--- pages/checkout/index.vue
+++ pages/checkout/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="my-10">
     <div class="text-center">
-      <BaseHeader text="Welcome in checkout" />
+      <BaseHeader text="Welcome in checkout" class="inline-block" />
     </div>
     <div class="mx-auto my-10 flex max-w-7xl flex-row gap-10">
       <CartItemsList class="flex-1" />
