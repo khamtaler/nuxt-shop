c4aa600e6d529c59770cc45ce2e2191b82aad18f
Tomasz Sroka <tsroka41@gmail.com>
2023-07-31 16:25:35 +0200

    fixing missing api with transformation of data to match all types, with right API this would not be neccessary...

--- composables/Product.ts
+++ composables/Product.ts
@@ -1,22 +1,36 @@
-import { ProductItem } from '@/types/components'
+import { CartProductItem, ProductItem } from '@/types/components'
 
 export const useProduct = () => {
-  const product = ref<ProductItem | null>(null)
+  const product = ref<CartProductItem | null>(null)
   const products = ref<ProductItem[] | null>(null)
   const wait = ref<boolean>(false)
   const error = ref<any>(undefined)
 
   const fetchProduct = async (id: string | string[]) => {
-    wait.value = true
-    try {
-      const res = await fetch(`https://fakestoreapi.com/products/${id}`)
-      const data = await res.json()
-      product.value = data
-    } catch (err: any) {
-      error.value = err
-    } finally {
-      wait.value = false
-    }
+    const { data, pending } = await useFetch<ProductItem>(
+      `https://fakestoreapi.com/products/${id}`,
+      {
+        server: true,
+        transform(data): CartProductItem {
+          return { ...data, count: 0 }
+        },
+      },
+    )
+    product.value = data.value as CartProductItem
+    wait.value = pending.value as boolean
+
+    // wait.value = true
+    // try {
+    //   const res = await fetch(`https://fakestoreapi.com/products/${id}`).then(
+    //     res => res.map(product => product)
+    //   )
+    //   const data = await res.json()
+    //   product.value = data
+    // } catch (err: any) {
+    //   error.value = err
+    // } finally {
+    //   wait.value = false
+    // }
   }
 
   const fetchProducts = async (numberOfProds?: number | string) => {
