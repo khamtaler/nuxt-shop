9f2e6d7d27f2e14a2dcd00608542f11239a434ad
Tomasz Sroka <tsroka41@gmail.com>
2023-08-10 18:06:21 +0200

    switchig whole website lang to en

--- app.vue
+++ app.vue
@@ -4,13 +4,13 @@
   </NuxtLayout>
   <Account />
   <MobileMenu />
-  <!-- <AddToCartModal /> -->
 </template>
 
-<script setup lang="ts">
-useHead({
-  htmlAttrs: {
-    lang: 'en',
-  },
-})
-</script>
+<!-- 
+przeniesc head lang do definenuxtconfig
+mozna przerobic booleany otwierające modale na propsy
+wywalic nulle z composables
+mozna zmienic count zamiast w produkcie to tworzyc obiekt w koszyku z produktem i dodawac mu count(total)
+zmienic base itemki do docelowych folderow -->
+
+<script setup lang="ts"></script>
